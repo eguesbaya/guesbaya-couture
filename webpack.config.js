e08d2d1d22c87f5bd16d347dcda8add780ad8605
eguesbaya <guesbaya@hotmail.fr>
2021-07-21 17:09:53 +0200

    Homepage banner and navbar

--- webpack.config.js
+++ webpack.config.js
@@ -18,13 +18,13 @@ Encore
          from: './assets/images',
 
          // optional target path, relative to the output dir
-         to: 'images/[path][name].[ext]',
+         //to: 'images/[path][name].[ext]',
 
          // if versioning is enabled, add the file hash too
          to: 'images/[path][name].[hash:8].[ext]',
 
          // only copy files matching this pattern
-         pattern: /\.(png|jpg|jpeg)$/
+         //pattern: /\.(png|jpg|jpeg)$/
      })
 
     /*
