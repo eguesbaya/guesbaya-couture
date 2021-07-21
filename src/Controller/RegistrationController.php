d95b210a885fbdf580b5d639f18cbe2ae6e3c8f6
eguesbaya <guesbaya@hotmail.fr>
2021-07-21 22:37:02 +0200

    removed email confirmation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,14 +45,16 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('no-reply@guesbaya-couture.fr', 'Emma'))
-                    ->to($user->getEmail())
-                    ->subject('Veuillez confirmer votre email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            $this->addFlash('warning', 'Votre compte a bien été créé.');
+
+            // // generate a signed url and email it to the user
+            // $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            //     (new TemplatedEmail())
+            //         ->from(new Address('no-reply@guesbaya-couture.fr', 'Emma'))
+            //         ->to($user->getEmail())
+            //         ->subject('Veuillez confirmer votre email')
+            //         ->htmlTemplate('registration/confirmation_email.html.twig')
+            // );
             // do anything else you need here, like send an email
 
             return $this->redirectToRoute('home');
