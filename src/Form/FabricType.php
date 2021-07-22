c182baeca8a731343e402a1e467c7eb88f5d51c6
eguesbaya <guesbaya@hotmail.fr>
2021-07-22 10:33:43 +0200

    edited style and routes. basic forms, need to be elaborated on

--- src/Form/FabricType.php
+++ src/Form/FabricType.php
@@ -5,17 +5,22 @@ namespace App\Form;
 use App\Entity\Fabric;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class FabricType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
-            ->add('name')
-            ->add('photo')
-            ->add('type')
-            ->add('priceByMetre')
+            ->add('name', TextType::class)
+            ->add('photo', UrlType::class)
+            ->add('type', TextType::class)
+            ->add('priceByMetre', NumberType::class )
         ;
     }
 
