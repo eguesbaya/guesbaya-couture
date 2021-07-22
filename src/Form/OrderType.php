47f900f1351ae52bd6efeda4d2f570634ce82693
eguesbaya <guesbaya@hotmail.fr>
2021-07-22 14:39:12 +0200

    created crud for orders

---
+++ src/Form/OrderType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('shippingAddress')
+            ->add('price')
+            ->add('pattern')
+            ->add('fabric')
+            ->add('measurements')
+            ->add('status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
