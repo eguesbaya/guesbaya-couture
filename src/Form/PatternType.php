e06d7194ba58ab9f0512d9874b891708b4200edc
eguesbaya <guesbaya@hotmail.fr>
2021-07-22 11:29:23 +0200

    make crud pattern

---
+++ src/Form/PatternType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pattern;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PatternType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('photo')
+            ->add('gender')
+            ->add('fabricQuantity')
+            ->add('Type')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Pattern::class,
+        ]);
+    }
+}
